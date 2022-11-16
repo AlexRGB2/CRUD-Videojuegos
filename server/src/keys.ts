2e9b09402fa2a3cc697254144ead091b82198514
AlexRGB <alexismtz200326@gmail.com>
2022-11-16 00:36:20 -0600

    Arreglando API

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,8 +1,9 @@
-export default{
-    database:{
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'ng_games_db'
-    }
-}
+export default {
+  database: {
+    host: "containers-us-west-125.railway.app",
+    user: "root",
+    password: "7GXecveJqBYdlOYuQZRf",
+    database: "railway",
+    port: "6771",
+  },
+};
